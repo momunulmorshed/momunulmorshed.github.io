06e94033bf69853c72f933492afb9f4adf414dde
Md.Momunul Morshed <hemal831@gmail.com>
2021-10-14 13:13:44 +0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $message = $_POST['message'];
 
 $mailheader = "From:".$name."<".$email.">\r\n";
 
-$recipient = "sofiullahc@gmail.com";
+$recipient = "momunulmorshed@gmail.com";
 
 mail($recipient, $subject, $message, $mailheader) or die("Error!");
 
